f75394a23aa5e4e87ed028007caaa35f0dee5e34
aqvila2k18 <aqvila.true@gmail.com>
2018-04-18 13:58:12 -0500

    script

--- vm2.sh
+++ vm2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 modprobe 8021q >> /dev/null 2>&1
 s_path=$(cd "$(dirname $0)" && pwd)
-source $s_path'/vm1.config'
+source $s_path'/vm2.config'
 echo 'source /etc/network/interfaces.d/*' > /etc/network/interfaces
 echo 'auto lo' >> /etc/network/interfaces
 echo 'iface lo inet loopback' >> /etc/network/interfaces
@@ -11,14 +11,28 @@ echo 'iface lo inet loopback' >> /etc/network/interfaces
 echo "auto $INTERNAL_IF" >> /etc/network/interfaces
 echo "iface $INTERNAL_IF inet static" >> /etc/network/interfaces
 echo "address $INT_IP" >> /etc/network/interfaces
+echo "gateway $GW_IP" >> /etc/network/interfaces
+echo "nameserver $GW_IP" >> /etc/resolv.conf
+echo 'nameserver 8.8.8.8' >> /etc/resolv.conf
 
 # Config VLAN
 
 echo "auto $INTERNAL_IF.$VLAN" >> /etc/network/interfaces
 echo "iface $INTERNAL_IF.$VLAN inet static" >> /etc/network/interfaces
-echo "address $VLAN_IP" >> /etc/network/interfaces
+echo "address $APACHE_VLAN_IP" >> /etc/network/interfaces
 echo "vlan_raw_device $INTERNAL_IF" >> /etc/network/interfaces
 
-ip addr flush $EXTERNAL_IF
+#delete start
+echo "auto $MANAGEMENT_IF" >> /etc/network/interfaces
+echo "iface $MANAGEMENT_IF inet static" >> /etc/network/interfaces
+echo 'address 192.168.56.102' >> /etc/network/interfaces
+#delete end
+
+ip addr flush $MANAGEMENT_IF
 ip addr flush $INTERNAL_IF
 systemctl restart networking.service
+
+# install apache2
+ping -c 5 -W 1 www.google.com
+apt-get update >> /dev/null 2>&1
+apt-get install apache2 >> /dev/null 2>&1

637efa718dcf490c1db8ddade2a13ee8b8add151
aqvila2k18 <36851646+aqvila2k18@users.noreply.github.com>
2018-04-18 23:45:45 +0300

    Update vm2.sh

--- vm2.sh
+++ vm2.sh
@@ -6,8 +6,7 @@ echo 'source /etc/network/interfaces.d/*' > /etc/network/interfaces
 echo 'auto lo' >> /etc/network/interfaces
 echo 'iface lo inet loopback' >> /etc/network/interfaces
 
-# Config INTERNAL_IF
-
+# config INTERNAL_IF
 echo "auto $INTERNAL_IF" >> /etc/network/interfaces
 echo "iface $INTERNAL_IF inet static" >> /etc/network/interfaces
 echo "address $INT_IP" >> /etc/network/interfaces
@@ -15,20 +14,13 @@ echo "gateway $GW_IP" >> /etc/network/interfaces
 echo "nameserver $GW_IP" >> /etc/resolv.conf
 echo 'nameserver 8.8.8.8' >> /etc/resolv.conf
 
-# Config VLAN
-
+# config VLAN
 echo "auto $INTERNAL_IF.$VLAN" >> /etc/network/interfaces
 echo "iface $INTERNAL_IF.$VLAN inet static" >> /etc/network/interfaces
 echo "address $APACHE_VLAN_IP" >> /etc/network/interfaces
 echo "vlan_raw_device $INTERNAL_IF" >> /etc/network/interfaces
 
-#delete start
-echo "auto $MANAGEMENT_IF" >> /etc/network/interfaces
-echo "iface $MANAGEMENT_IF inet static" >> /etc/network/interfaces
-echo 'address 192.168.56.102' >> /etc/network/interfaces
-#delete end
-
-ip addr flush $MANAGEMENT_IF
+ip addr flush $INTERNAL_IF.$VLAN
 ip addr flush $INTERNAL_IF
 systemctl restart networking.service
 
